7994d2d6ecad7f36c797016bf2112e0c86dab2ec
luan11 <luaannovais@live.com>
2020-09-20 11:52:13 -0300

    O arquivo tsconfig.json

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const num1 = document.getElementById('num1') as HTMLInputElement;
 const num2 = document.getElementById('num2') as HTMLInputElement;
-const button = document.getElementById('button');
+const button = document.getElementById('button')!;
 
 function sum(a: number, b: number) {
 	return a + b;
